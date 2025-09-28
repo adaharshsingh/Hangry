5dee585cdece6db2fccb0a383a25044a3646a4e5
Adarsh <Mr.aadarshkumarsingh@gmail.com>
2025-09-29 01:21:38 +0530

    bug

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -15,7 +15,7 @@ const AppRoutes = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/register" element={<ChooseRegister />} />
+                <Route path="/" element={<ChooseRegister />} />
                 <Route path="/user/register" element={<UserRegister />} />
                 <Route path="/user/login" element={<UserLogin />} />
                 <Route path="/food/register" element={<FoodPartnerRegister />} />
